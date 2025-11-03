7964d09e2986b658d332b9d7cd257836ea457777
slasherdrake <dkdrakekendall@gmail.com>
2025-11-02 21:20:06 -0700

    added links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 
 import luffy from '../assets/luffy.png';
+import LinkedIn from '../assets/LI-Logo.png';
+import Github from '../assets/GitHub_Logo_White.png';
 export default function Home(){
     return (
         <div className='container-xl'>
@@ -7,6 +9,18 @@ export default function Home(){
             <div className='bar'></div>
             <h2 className='green'>a Software Developer and Master's student in Software</h2>
             <h2 className='green'>Engineering at the University of Arizona. Welcome to my website!</h2>
+            <h2 className ='green'>Contact: drakekendall@email.com</h2>
+            <button className = 'link-button'>
+                <a href="https://www.linkedin.com/in/drake-kendall/" target="_blank" rel="noopener noreferrer">
+                <img src={LinkedIn} alt='LinkedIn Link'></img>
+                </a>
+            </button>
+            <button className = 'link-button'>
+                <a href="https://github.com/slasherdrake" target="_blank" rel="noopener noreferrer">
+                <img src={Github} alt='Github link'></img>
+                </a>
+            </button>
+        
             
         </div>
     )
